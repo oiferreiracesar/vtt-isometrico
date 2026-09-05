8c04cfe77807d989ef88c673c0364b09248a0852
oiferreiracesar <ceesarsantosjunior@gmail.com>
2026-09-05 12:44:25 -0300

    Update mapa.js

--- js/mapa.js
+++ js/mapa.js
@@ -1,53 +1,40 @@
 // js/mapa.js - Gerenciamento do chão e dimensões do tabuleiro
 import { scene } from './engine.js';
 
-export let configMapa = {
-  largura: 32,      // Número de Colunas
-  profundidade: 18, // Número de Linhas
-  tamanhoGrid: 1
-};
-
+export let configMapa = { largura: 32, profundidade: 18, tamanhoGrid: 1 };
 export let meshChaoBase;
 let gridHelper;
 
-export function iniciarMapa() {
-  reconstruirGrid();
-}
+export function iniciarMapa() { reconstruirGrid(); }
 
-// Recria a malha do chão e o desenho das linhas quando o Mestre altera o tamanho
 export function redimensionarMapa(novaLargura, novaProfundidade) {
-  configMapa.largura = novaLargura;
-  configMapa.profundidade = novaProfundidade;
+  configMapa.largura = novaLargura; configMapa.profundidade = novaProfundidade;
   reconstruirGrid();
 }
 
 export function reconstruirGrid() {
-  // Limpa o chão antigo se existir
   if (meshChaoBase) scene.remove(meshChaoBase);
   if (gridHelper) scene.remove(gridHelper);
   
-  // Cria o plano de colisão (onde o mouse "toca")
   const materialChaoBase = new THREE.MeshLambertMaterial({ color: 0x2a2620 });
   meshChaoBase = new THREE.Mesh(new THREE.PlaneGeometry(configMapa.largura, configMapa.profundidade), materialChaoBase);
   meshChaoBase.rotation.x = -Math.PI / 2;
   meshChaoBase.position.y = 0;
+  
+  // O CHÃO AGORA RECEBE SOMBRAS!
+  meshChaoBase.receiveShadow = true; 
+  
   scene.add(meshChaoBase);
 
-  // Desenha as linhas da grade
   const pontos = [];
-  const meiaLargura = configMapa.largura / 2;
-  const meiaProfundidade = configMapa.profundidade / 2;
+  const meiaLargura = configMapa.largura / 2, meiaProfundidade = configMapa.profundidade / 2;
   const t = configMapa.tamanhoGrid;
 
-  for (let x = -meiaLargura; x <= meiaLargura + 1e-6; x += t) {
-    pontos.push(x, 0.01, -meiaProfundidade, x, 0.01, meiaProfundidade);
-  }
-  for (let z = -meiaProfundidade; z <= meiaProfundidade + 1e-6; z += t) {
-    pontos.push(-meiaLargura, 0.01, z, meiaLargura, 0.01, z);
-  }
+  for (let x = -meiaLargura; x <= meiaLargura + 1e-6; x += t) { pontos.push(x, 0.01, -meiaProfundidade, x, 0.01, meiaProfundidade); }
+  for (let z = -meiaProfundidade; z <= meiaProfundidade + 1e-6; z += t) { pontos.push(-meiaLargura, 0.01, z, meiaLargura, 0.01, z); }
 
   const geo = new THREE.BufferGeometry();
   geo.setAttribute('position', new THREE.Float32BufferAttribute(pontos, 3));
-  gridHelper = new THREE.LineSegments(geo, new THREE.LineBasicMaterial({ color: 0x4a4030 }));
+  gridHelper = new THREE.LineSegments(geo, new THREE.LineBasicMaterial({ color: 0x4a4030, transparent: true, opacity: 0.5 }));
   scene.add(gridHelper);
 }
