228ca5f4839749a64801ea40aec337f361c195ab
oiferreiracesar <ceesarsantosjunior@gmail.com>
2026-09-08 00:47:26 -0300

    update.

--- js/construtor.js
+++ js/construtor.js
@@ -514,10 +514,15 @@ export function reconstruirTelhadoPiramide(telhado) {
         geo.rotateY(Math.PI / 8); geo.scale(0.765366, 1, 0.765366);
     }
     
+    // CORREÇÃO CRÍTICA: Atualiza o "hitbox" invisível do telhado para o mouse não passar reto
+    geo.computeBoundingBox();
+    geo.computeBoundingSphere();
+    
     const matBase = telhado.textura ? gerarMaterialPintura(telhado.textura, Math.max(1, w/configMapa.tamanhoGrid), Math.max(1, d/configMapa.tamanhoGrid)) : materialTelhadoPadrão.clone();
     const matBottom = new THREE.MeshBasicMaterial({ color: 0x000000, visible: false });
     const pyramid = new THREE.Mesh(geo, [matBase, matBottom]);
     pyramid.scale.set(w, h, d);
+    pyramid.updateMatrixWorld(); // Força o motor a reconhecer a nova escala física
     
     const alturaBase = (telhado.nivel * obterAltura()) + obterAltura();
     telhado.mesh.position.set((telhado.ax + telhado.bx)/2, alturaBase + (h / 2), (telhado.az + telhado.bz)/2);
